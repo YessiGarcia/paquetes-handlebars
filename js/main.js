75cbef5d8ea2d92cec480a95a63e476c90106d45
YessiGarcia <aria.801900@gmail.com>
2017-07-27 22:27:27 -0500

    inicio de maqueta

---
+++ js/main.js
@@ -0,0 +1,5 @@
+/*var template = document.getElementById("index").innerHTML;
+    console.log(template);
+    var compilar = Handlebars.compile(template);
+    var compiladorHTML = compilar ({name: "<em><p>Pari</p></em>"});
+    document.getElementById("action").innerHTML += compiladorHTML;*/
